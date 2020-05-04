74785a5eacc4fb24c2e90cd5c135c80107e9056c
matiasnauel <ayala.matias.ma@gmail.com>
2020-05-04 02:03:34 -0300

    Primero Commit

---
+++ API-CAPA-DATO/DatoDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace API_CAPA_DATO
+{
+    class DatoDbContext :DbContext
+    {
+
+    }
+}
---
+++ API-CAPA-DOMINIO/Entity/Producto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace API_CAPA_DOMINIO.Entity
+{
+    class Producto
+    {
+    }
+}
